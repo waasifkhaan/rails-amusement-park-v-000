7660828dbaecc50d41f9fd16aff37b1c9dbb436f
Wasif Khan <waasif.khaan@gmail.com>
2018-10-20 18:23:19 +0000

    automatically backed up by learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,12 +3,14 @@ class Ride < ApplicationRecord
   belongs_to :attraction
 
   def take_ride
+    binding.pry
     if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
     "Sorry. You do not have enough tickets to ride the Roller Coaster. You are not tall enough to ride the Roller Coaster."
     elsif self.user.tickets < self.attraction.tickets
       "Sorry. You do not have enough tickets to ride the Roller Coaster."
     elsif self.user.height < self.attraction.min_height
       "Sorry. You are not tall enough to ride the Roller Coaster."
+      
     elsif self.user.tickets > self.attraction.tickets && self.user.height > self.attraction.min_height
       self.user.update(tickets: self.user.tickets - self.attraction.tickets , happiness: self.user.happiness + self.attraction.happiness_rating,
       nausea: self.user.nausea + self.attraction.nausea_rating )
