f15edf024b409515ef30a64486c34646ba8ff22e
Wasif Khan <waasif.khaan@gmail.com>
2018-10-20 18:15:24 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,7 +3,7 @@ class RidesController < ApplicationController
   def create
     binding.pry
     ride = Ride.new(attraction_id: params[:attraction_id], user_id: current_user.id)
-    @message = ride.take_ride
+    session[:error] = ride.take_ride
     redirect_to user_path(current_user)
   end
 end
