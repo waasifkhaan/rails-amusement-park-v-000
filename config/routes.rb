80456147d0e14e7f7cedfb59eca73b43b7dd4f5a
Wasif Khan <waasif.khaan@gmail.com>
2018-10-20 17:47:10 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-  post '' 
+  post '/rides' => 'rides#create'
 end
