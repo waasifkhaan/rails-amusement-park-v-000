0aeef7c1a8831375bdaa4df1dfc3102e08a09820
Wasif Khan <waasif.khaan@gmail.com>
2018-10-20 16:38:36 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
 
     @user = User.new(user_params)
-    @user.admin = params[:admin]
+  
     if @user.save
 
       session[:user_id] = @user.id
