5fcfb6eb3b2b2c7cb73bb1d1fbbffe3782cc198e
Wasif Khan <waasif.khaan@gmail.com>
2018-10-20 18:23:23 +0000

    automatically backed up by learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -10,7 +10,7 @@ class Ride < ApplicationRecord
       "Sorry. You do not have enough tickets to ride the Roller Coaster."
     elsif self.user.height < self.attraction.min_height
       "Sorry. You are not tall enough to ride the Roller Coaster."
-      
+
     elsif self.user.tickets > self.attraction.tickets && self.user.height > self.attraction.min_height
       self.user.update(tickets: self.user.tickets - self.attraction.tickets , happiness: self.user.happiness + self.attraction.happiness_rating,
       nausea: self.user.nausea + self.attraction.nausea_rating )
