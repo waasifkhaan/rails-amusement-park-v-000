9f27be00db93b179670a6cbbca549f524dd305df
Wasif Khan <waasif.khaan@gmail.com>
2018-10-20 18:08:14 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,11 +3,11 @@ class RidesController < ApplicationController
   def create
     binding.pry
     ride = Ride.new(attraction_id: params[:attraction_id], user_id: current_user.id)
-    
+
     if ride.take_ride
 
 
   end
-end 
+end
 
 end
