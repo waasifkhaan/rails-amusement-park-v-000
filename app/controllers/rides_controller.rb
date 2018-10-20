b8cad4114a789450a41534ed0d64932759a9a247
Wasif Khan <waasif.khaan@gmail.com>
2018-10-20 18:19:12 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,7 +1,7 @@
 class RidesController < ApplicationController
 
   def create
-    
+
     ride = Ride.new(attraction_id: params[:attraction_id], user_id: current_user.id)
     session[:error] = ride.take_ride
     redirect_to user_path(current_user)
